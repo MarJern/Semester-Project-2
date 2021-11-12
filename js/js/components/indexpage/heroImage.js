23ba08b5ea2e6368d27473c84b173b9dbbee8d33
MarJern <72491062+MarJern@users.noreply.github.com>
2021-11-12 09:22:20 +0100

    Add files via upload

--- js/js/components/indexpage/heroImage.js
+++ js/js/components/indexpage/heroImage.js
@@ -1,5 +1,6 @@
-import { baseUrl } from "../../constants/api.js";
+import { baseUrl} from "../../constants/api.js";
 import displayMessage from "../displayMessage.js";
+
 
 const heroUrl = baseUrl + "/home";
 
@@ -8,11 +9,9 @@ export async function getHeroImage() {
     try{
         const response = await fetch(heroUrl);
         const json = await response.json();
-        console.log(json);
-        console.log(json.hero_banner.url);
 
         heroContainer.innerHTML += 
-            `<img class="card__image mb-2" src="${baseUrl + json.hero_banner.url}" alt="">`;
+            `<img class="hero__image mb-2 w-100 mb-0" src="${baseUrl + json.hero_banner.url}" alt="">`;
     }catch(error) {
         displayMessage("error", "Something went wrong when loading the content", ".hero__container");
     };
